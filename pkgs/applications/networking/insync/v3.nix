0fa91456d2f6dfb9cd4008e81c89c2fec8512415
Miao, ZhiCheng <miao@superfluid.finance>
2022-12-13 20:58:51 +0200

    insync-v3: fix build
    - Download latest binary from Ubuntu jammy, and delete dynamic libraries,
    excluding PySide2 bindings and plugins.
      - NB! Ubuntu kinetic binary is not yet usable because it uses glibc 2.36.
    - set hellwolf as maintainer of insync-v3
    
    (cherry picked from commit 03c01b729993503ec305de2c365cae6b1eaf1891)

--- pkgs/applications/networking/insync/v3.nix
+++ pkgs/applications/networking/insync/v3.nix
@@ -1,30 +1,31 @@
-{ stdenv
-, lib
+{ lib
+, stdenv
 , fetchurl
 , makeWrapper
 , dpkg
-, libxcb
-, libGL
-, nss
-, libthai
-, wayland
-, alsa-lib
-, qtvirtualkeyboard
-, qtwebchannel
-, qtwebsockets
-, qtlocation
-, qtwebengine
 , autoPatchelfHook
-, wrapQtAppsHook
+# libs
+, python37
+, python37Packages
+, readline81
+, curl
+, gobject-introspection
+, gtk3
+, libthai
+, qt5 # qt5 packages
 }:
 
 stdenv.mkDerivation rec {
   pname = "insync";
-  version = "3.3.5.40925";
+  version = "3.8.4.50481";
 
   src = fetchurl {
-    url = "http://s.insynchq.com/builds/${pname}_${version}-focal_amd64.deb";
-    sha256 = "sha256-lYlG/8d7teX98F5eDxm4EdBfFs7Sz3Td4kKLC6KZqnQ=";
+    # Find a binary from https://www.insynchq.com/downloads/linux#ubuntu.
+    #
+    # Note: Ubuntu kinetic (22.10) uses glibc 2.36, which nixpkgs does not use yet.
+    #       So we use jammy package instead.
+    url = "https://cdn.insynchq.com/builds/linux/${pname}_${version}-jammy_amd64.deb";
+    sha256 = "sha256-jd2/XB9UYhofvI7rDrjCTXtjJyo62JQtPo2t53kTjRs=";
   };
 
   postPatch = ''
@@ -32,42 +33,82 @@ stdenv.mkDerivation rec {
   '';
 
   buildInputs = [
-    alsa-lib
-    libGL
+    python37
+    readline81
+    curl
+    gobject-introspection
+    gtk3
     libthai
-    libxcb
-    nss
-    qtlocation
+  ] ++ (with qt5; [
+    qtbase
     qtvirtualkeyboard
+    qtlocation
     qtwebchannel
     qtwebengine
     qtwebsockets
-    wayland
-  ];
+    qtserialport
+    qtwayland
+  ]) ++ (with python37Packages; [
+    pyside2
+    shiboken2
+  ]);
 
-  nativeBuildInputs = [ autoPatchelfHook dpkg makeWrapper wrapQtAppsHook ];
+  nativeBuildInputs = [ autoPatchelfHook dpkg makeWrapper ];
 
   unpackPhase = ''
     dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin $out/lib $out/share
+    # delete dynamic libraries
+    ls usr/lib/insync/lib{*.so,*.so.*} | xargs rm -f
+    rm usr/lib/insync/nss -r
+    rm usr/lib/insync/shiboken2/ -r
+    rm usr/lib/insync/PySide2/ -r
+    # copy files and link necessary modules
     cp -R usr/* $out/
-    rm $out/lib/insync/libGLX.so.0
-    rm $out/lib/insync/libQt5*
-    sed -i 's|/usr/lib/insync|/lib/insync|' "$out/bin/insync"
-    wrapQtApp "$out/lib/insync/insync"
+    ln -s ${python37}/lib/libpython3.7m.so $out/lib/insync/libpython3.7m.so.1.0
+    ln -s ${python37Packages.shiboken2}/lib/python3.7/site-packages/shiboken2 $out/lib/insync/shiboken2
+    mkdir $out/lib/insync/PySide2/
+    ln -s ${python37Packages.pyside2}/lib/python3.7/site-packages/PySide2/* $out/lib/insync/PySide2/
+  ''
+  # The QT shipped with insync doesn't seem to respect QT_PLUGIN_PATH env var, hence wrapQtApp is moot.
+  # Here is a hack to symlink all plugins to insync libs folder.
+  + (builtins.concatStringsSep "\n" (builtins.map (pkg: let
+      pluginPath = "${pkg.bin or pkg}/${qt5.qtbase.qtPluginPrefix}";
+      qmlPath = "${pkg.bin or pkg}/${qt5.qtbase.qtQmlPrefix}";
+    in ''
+      [ -d ${pluginPath} ] && find -L ${pluginPath} -type f -printf "%P\n" | while read i;do
+        mkdir -p $out/lib/insync/PySide2/plugins/$(dirname $i)
+        ln -s ${pluginPath}/$i $out/lib/insync/PySide2/plugins/$i
+      done
+      [ -d ${qmlPath} ] && find -L ${qmlPath} -type f -printf "%P\n" | while read i;do
+        mkdir -p $out/lib/insync/PySide2/qml/$(dirname $i)
+        ln -s ${qmlPath}/$i $out/lib/insync/PySide2/qml/$i
+      done
+    '') buildInputs))
+  + ''
+    # fix the launcher
+    rm -f "$out/bin/insync"
+    makeWrapper $out/lib/insync/insync $out/bin/insync \
+      --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+      --prefix XDG_DATA_DIRS : $out/share
+
+    runHook postInstall
   '';
 
   dontConfigure = true;
   dontBuild = true;
+  dontWrapQtApps = true;
 
   meta = with lib; {
     platforms = ["x86_64-linux"];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
-    maintainers = with maintainers; [ benley ];
+    maintainers = with maintainers; [ hellwolf kaesaecracker ];
     homepage = "https://www.insynchq.com";
     description = "Google Drive sync and backup with multiple account support";
     longDescription = ''
@@ -78,7 +119,5 @@ stdenv.mkDerivation rec {
 
      There is a 15-day free trial, and it is a paid application after that.
     '';
-    # download URL removed
-    broken = true;
   };
 }
